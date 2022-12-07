555a617f8f4b893d6083ecc0670906580ddc025a
jeffthebrink <jeffthebrink@gmail.com>
2022-12-07 11:37:24 -0700

    use state for user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from "./Header";
 import UserInput from "./UserInput";
 import './App.css';
--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UserInput() {
+    const [url, setUrl] = useState<any>('');
+
+    const handleSubmit = () => {
+        window.alert(url);
+    }
+
     return (
         <div className="url-input">
             <h3>Enter a URL to fetch from</h3>
             <span>
-                <input inputMode="text" type="text" id="url-input" required/>
-                <button onClick={() => window.alert('submitted')}>Submit</button>
+                <input inputMode="text" type="text" id="url-input" value={url} required onChange={event => setUrl(event.target.value)}/>
+                <button onClick={handleSubmit}>Submit</button>
             </span>
         </div>
     );
