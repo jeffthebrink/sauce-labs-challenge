06454a358cbcea95887dfa6959c6a9b0f3dedf57
jeffthebrink <jeffthebrink@gmail.com>
2022-12-08 14:23:44 -0700

    implement clear feature

--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -1,28 +1,56 @@
-import React, { useState } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Response from "./Response";
 
 function UserInput() {
     const [url, setUrl] = useState<string>('');
     const [response, setResponse] = useState<any>(null);
+    const [showResponse, setShowResponse] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const handleSubmit = async () => {
+    const handleSubmit = useCallback(async () => {
         if (url.length < 1) return;
+        setLoading(true);
         const response = await fetch(url, {
             method: 'GET',
             headers: {},
         });
         const content = await response.json();
         setResponse(content);
-    }
+        setLoading(false);
+        setShowResponse(true);
+    }, [url]);
+
+    useEffect(() => {
+        const enterListener = (e: any) => {
+            if (e.key === 'Enter') {
+                handleSubmit().then(null);
+            }
+        };
+        document.addEventListener('keypress', enterListener);
+
+        return () => document.removeEventListener('keypress', enterListener);
+    }, [handleSubmit]);
 
     return (
         <div className="url-input-container">
             <h3>Enter a URL to fetch from</h3>
             <span>
-                <input className="input-field" inputMode="text" type="text" id="url-input" value={url} required onChange={event => setUrl(event.target.value)}/>
-                <button className="submit-btn" onClick={handleSubmit}>Submit</button>
+                <input
+                    className="input-field"
+                    inputMode="text"
+                    type="text"
+                    id="url-input"
+                    value={url}
+                    required
+                    onChange={event => setUrl(event.target.value)}
+                />
+                <button className="submit-btn" onClick={handleSubmit}>{loading ? 'Loading' : 'Submit'}</button>
+                <button className="clear-btn" onClick={() => {
+                    setUrl('');
+                    setShowResponse(false);
+                }}>Clear</button>
             </span>
-            {response && <Response response={response} url={url} />}
+            {response && showResponse && <Response response={response} url={url}/>}
         </div>
     );
 }
