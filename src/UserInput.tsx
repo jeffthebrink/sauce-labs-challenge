00e2b20531f278802898db4b46ac6e1f5cbfe077
jeffthebrink <jeffthebrink@gmail.com>
2022-12-21 10:26:13 -0700

    handle state through useReducer

--- src/UserInput.tsx
+++ src/UserInput.tsx
@@ -1,23 +1,46 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState, useReducer} from 'react';
 import Response from "./Response";
+
+interface DataRemote {
+    status: 'success' | 'failure' | 'loading' | 'not-requested';
+    data: null | string | any;
+}
+
+const initialState: DataRemote = {status: 'not-requested', data: null};
+
+function reducer(state: any, action: { type: string, payload: any }) {
+    switch (action.type) {
+        case 'success':
+            return {status: 'success', data: action.payload};
+        case 'failure':
+            return {status: 'failure', data: 'Error fetching data'};
+        case 'loading':
+            return {status: 'loading', data: null};
+        case 'not-requested':
+            return initialState;
+        default:
+            throw new Error();
+    }
+}
 
 function UserInput() {
     const [url, setUrl] = useState<string>('');
-    const [response, setResponse] = useState<any>(null);
-    const [showResponse, setShowResponse] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     const handleSubmit = useCallback(async () => {
         if (url.length < 1) return;
-        setLoading(true);
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {},
-        });
-        const content = await response.json();
-        setResponse(content);
-        setLoading(false);
-        setShowResponse(true);
+        dispatch({type: 'loading', payload: null});
+        let response = null;
+        try {
+            response = await fetch(url, {
+                method: 'GET',
+                headers: {},
+            });
+            const content = await response.json();
+            dispatch({type: 'success', payload: content});
+        } catch (error) {
+            dispatch({type: 'failure', payload: null});
+        }
     }, [url]);
 
     useEffect(() => {
@@ -31,8 +54,25 @@ function UserInput() {
         return () => document.removeEventListener('keypress', enterListener);
     }, [handleSubmit]);
 
+    const renderStatus = () => {
+        switch (state.status) {
+            case 'success':
+                return <Response response={state.data} url={url}/>
+            case 'failure':
+                return (
+                    <div className={'fetch-error'}>
+                        <p>{state.data}</p>
+                    </div>
+                )
+            case 'loading':
+                return <div className={'loading'}>Loading...</div>
+            case 'not-requested':
+                return <></>
+        }
+    }
+
     return (
-        <div className="url-input-container">
+        <div className="url-input-and-response-container">
             <h3>Enter a URL to fetch from</h3>
             <span>
                 <input
@@ -44,14 +84,15 @@ function UserInput() {
                     required
                     onChange={event => setUrl(event.target.value)}
                 />
-                <button className="submit-btn" onClick={handleSubmit}>{loading ? 'Loading' : 'Submit'}</button>
+                <button className="submit-btn"
+                        onClick={handleSubmit}>{state.status === 'loading' ? 'Loading' : 'Submit'}</button>
                 <button className="clear-btn" onClick={() => {
                     setUrl('');
-                    setShowResponse(false);
+                    dispatch({type: 'not-requested', payload: null});
                 }}>Clear</button>
             </span>
             <hr/>
-            {response && showResponse && <Response response={response} url={url}/>}
+            {renderStatus()}
         </div>
     );
 }
